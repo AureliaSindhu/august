cb0439088848b92c5ddf506c1578330c5fb20cc4
AureliaSindhu <aurelia.sindhu@gmail.com>
2025-08-12 21:34:29 +0700

    branding sort of

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,8 @@
 export default function Footer() {
     return (
-        <div className="max-w-xl mx-auto p-4 flex flex-col items-center justify-center text-sm text-tertiary">
-            made with several cups of americano, love <span className="inline-flex items-center gap-1 text-accent font-bold">aacode</span>
+        <div className="max-w-xl mx-auto p-4 text-center items-center justify-center text-sm text-tertiary">
+            <p>made with several cups of americano, love {" "} <span className="inline-flex items-center gap-1 text-accent font-bold"> aacode</span></p>
+            <p> est. &copy; Aug 2025, All rights reserved</p>
         </div>
     );
 }
