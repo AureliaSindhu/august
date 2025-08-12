4355a99af8022c941416a96b4b28ac4723daccef
AureliaSindhu <aurelia.sindhu@gmail.com>
2025-08-12 21:32:53 +0700

    fix placeholder

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,14 +9,14 @@ export default function Hero() {
                     #myAugustMood
                 </h1>
 
-                <p className="mx-auto mt-4 mb-8 max-w-xl text-pretty text-sm text-white/70 sm:text-base">
-                    Transform your August feelings into dreamy visuals—like folklore&apos;s golden hour, distilled.
+                <p className="mx-auto m-4 max-w-xl text-pretty text-sm text-white/70 sm:text-base">
+                    Transform your August feelings into dreamy visuals, like folklore&apos;s golden hour, distilled.
                 </p>
 
-                <CometCard className="flex items-center justify-center m-12">
+                <CometCard className="flex items-center justify-center m-10">
                     <button
                         type="button"
-                        className="flex w-80 cursor-pointer flex-col items-stretch rounded-[16px] border-0 bg-[#1F2121] p-2 saturate-0 md:p-4"
+                        className="flex w-70 cursor-pointer flex-col items-stretch rounded-[16px] border-0 bg-[#1F2121] p-2 saturate-0 md:p-4"
                         aria-label="View invite F7RA"
                         style={{
                             transformStyle: "preserve-3d",
@@ -27,16 +27,16 @@ export default function Hero() {
                         <div className="mx-2 flex-1">
                             <div className="relative aspect-[3/4] w-full">
                                 <img
-                                loading="lazy"
-                                className="absolute inset-0 h-full w-full rounded-[16px] bg-[#000000] object-cover contrast-75"
-                                alt="Invite background"
-                                src="https://images.unsplash.com/photo-1505506874110-6a7a69069a08?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                    loading="lazy"
+                                    className="absolute inset-0 h-full w-full rounded-[16px] bg-[#000000] object-cover contrast-75"
+                                    alt="Folklore"
+                                    src="/folklore.png"
                                 />
                             </div>
                         </div>
                         <div className="flex flex-shrink-0 items-center justify-between p-4 font-mono text-white">
-                        <div className="text-xs">Comet Invitation</div>
-                        <div className="text-xs text-gray-300 opacity-50">#F7RA</div>
+                        <div className="text-xs">Folklore</div>
+                        <div className="text-xs text-gray-300 opacity-50">#myAugustMood</div>
                         </div>
                     </button>
                 </CometCard>
