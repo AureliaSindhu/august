a0b6ee4121c71446ad40a604bcd5610ad45ad444
AureliaSindhu <aurelia.sindhu@gmail.com>
2025-08-13 08:11:29 +0700

    weird bg spacing

--- components/ui/background.tsx
+++ components/ui/background.tsx
@@ -6,7 +6,7 @@ interface BackgroundProps {
 
 export default function Background({ children }: BackgroundProps) {
     return (
-        <div className="relative w-full h-full">
+        <div className="relative w-full h-full min-h-screen">
         <div className="absolute inset-0 bg-gradient-to-b from-primary via-secondary to-tertiary opacity-50" />
         <div className="relative z-10">
             {children}
